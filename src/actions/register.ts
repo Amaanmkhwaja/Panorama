69e3b369296e134e6c0357728442ba8cea3abb57
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2024-02-08 15:20:57 -0500

    installed zod, created Login/Register zod schemas, created register server action

---
+++ src/actions/register.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: verification token and send email
+
+  return { success: "Confirmation email sent!" };
+};
---
+++ src/schemas/index.ts
@@ -0,0 +1,24 @@
+import { UserRole } from "@prisma/client";
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+  // dont use a min for login, only for register in case password requirements change in the future
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum of 6 characters required",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
