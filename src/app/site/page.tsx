bafe93d0ddde2e900f71129128ca6dcb4156dec1
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2024-02-09 16:31:54 -0500

    lint

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
@@ -13,16 +11,6 @@ export default function Home() {
           <h1 className="text-9xl font-bold text-center md:text-[300px]">
             Plura
           </h1>
-        </div>
-        <div className="flex justify-center items-center relative md:mt-[-70px]">
-          <Image
-            src={"/assets/preview.png"}
-            alt="banner image"
-            height={1200}
-            width={1200}
-            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
-          />
-          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
     </>
