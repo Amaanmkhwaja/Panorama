526fadfc8b8bcf38d0f13abb62a7408c5f8afed2
Amaanmkhwaja <amaanmkhwaja@gmail.com>
2024-02-13 01:17:59 -0500

    finished uploadthing reacthelpers

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -2,8 +2,11 @@ import {
   generateUploadButton,
   generateUploadDropzone,
 } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
