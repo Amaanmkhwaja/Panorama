4c3f14b2e44e3dc51dd126a4c8f643aa1cc69e0d
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2024-02-14 18:39:44 -0500

    Started Agency Team page

--- src/app/(main)/agency/[agencyId]/(routes)/team/page.tsx
+++ src/app/(main)/agency/[agencyId]/(routes)/team/page.tsx
@@ -1,10 +1,53 @@
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+
 interface TeamPageProps {
   params: {
     agencyId: string;
   };
 }
 
-const TeamPage = ({ params }: TeamPageProps) => {
+const TeamPage = async ({ params }: TeamPageProps) => {
+  const authUser = await currentUser();
+  const teamMembers = await db.userDetails.findMany({
+    where: {
+      Agency: {
+        id: params.agencyId,
+      },
+    },
+    include: {
+      Agency: { include: { SubAccount: true } },
+      Permissions: { include: { SubAccount: true } },
+    },
+  });
+
+  if (!authUser) return null;
+  const agencyDetails = await db.agency.findUnique({
+    where: {
+      id: params.agencyId,
+    },
+    include: {
+      SubAccount: true,
+    },
+  });
+
+  if (!agencyDetails) return;
+
+  // return (
+  //   <DataTable
+  //     actionButtonText={
+  //       <>
+  //         <Plus size={15} />
+  //         Add
+  //       </>
+  //     }
+  //     modalChildren={<SendInvitation agencyId={agencyDetails.id} />}
+  //     filterValue="name"
+  //     columns={columns}
+  //     data={teamMembers}
+  //   />
+  // )
+
   return <div>TeamPage</div>;
 };
 
