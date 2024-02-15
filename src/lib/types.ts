37980d0f0070de032c11c6a7cc2b8b6462417088
Amaanmkhwaja <amaanmkhwaja@gmail.com>
2024-02-15 01:26:51 -0500

    added __getUsersWithAgencySubAccountPermissionsSidebarOptions function

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,7 @@
 import { Notification, Prisma, UserRole } from "@prisma/client";
 import { getAuthUserDetails } from "@/data/user";
 import { getUserPermissions } from "@/actions/user";
+import { db } from "./db";
 
 export type NotificationWithUser =
   | ({
@@ -23,3 +24,20 @@ export type UserWithPermissionsAndSubAccounts = Prisma.PromiseReturnType<
 
 export type AuthUserWithAgencySibebarOptionsSubAccounts =
   Prisma.PromiseReturnType<typeof getAuthUserDetails>;
+
+const __getUsersWithAgencySubAccountPermissionsSidebarOptions = async (
+  agencyId: string
+) => {
+  return await db.userDetails.findFirst({
+    where: { Agency: { id: agencyId } },
+    include: {
+      Agency: { include: { SubAccount: true } },
+      Permissions: { include: { SubAccount: true } },
+    },
+  });
+};
+
+export type UsersWithAgencySubAccountPermissionsSidebarOptions =
+  Prisma.PromiseReturnType<
+    typeof __getUsersWithAgencySubAccountPermissionsSidebarOptions
+  >;
