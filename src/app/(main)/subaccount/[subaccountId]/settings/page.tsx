270704ced19225d3d11f5e6fced9070a160c2eb5
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2024-02-15 23:55:55 -0500

    created Sub Account Settings Page

--- src/app/(main)/subaccount/[subaccountId]/settings/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/settings/page.tsx
@@ -1,3 +1,8 @@
+import { SubAccountDetails } from "@/components/forms/subaccount-details";
+import { UserDetails } from "@/components/forms/user-details";
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+
 interface SubaccountSettingsPageProps {
   params: { subaccountId: string };
 }
@@ -5,7 +10,44 @@ interface SubaccountSettingsPageProps {
 const SubaccountSettingsPage = async ({
   params,
 }: SubaccountSettingsPageProps) => {
-  return <div>Sub Account Settins Page</div>;
+  const authUser = await currentUser();
+  if (!authUser) return;
+  const userDetails = await db.userDetails.findUnique({
+    where: {
+      email: authUser.email!,
+    },
+  });
+  if (!userDetails) return;
+
+  const subAccount = await db.subAccount.findUnique({
+    where: { id: params.subaccountId },
+  });
+  if (!subAccount) return;
+
+  const agencyDetails = await db.agency.findUnique({
+    where: { id: subAccount.agencyId },
+    include: { SubAccount: true },
+  });
+
+  if (!agencyDetails) return;
+  const subAccounts = agencyDetails.SubAccount;
+
+  return (
+    <div className="flex flex-col lg:!flex-row gap-4">
+      <SubAccountDetails
+        agencyDetails={agencyDetails}
+        details={subAccount}
+        userId={userDetails.id}
+        userName={userDetails.name}
+      />
+      <UserDetails
+        type="subaccount"
+        id={params.subaccountId}
+        subAccounts={subAccounts}
+        userData={userDetails}
+      />
+    </div>
+  );
 };
 
 export default SubaccountSettingsPage;
