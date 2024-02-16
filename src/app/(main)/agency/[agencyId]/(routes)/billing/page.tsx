adcf313c5d8e2d554a4f1f0218e68016d5918515
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2024-02-16 11:24:07 -0500

    Created Agency Billing page

--- src/app/(main)/agency/[agencyId]/(routes)/billing/page.tsx
+++ src/app/(main)/agency/[agencyId]/(routes)/billing/page.tsx
@@ -1,11 +1,174 @@
+import { db } from "@/lib/db";
+import { cn } from "@/lib/utils";
+import { stripe } from "@/lib/stripe";
+import { addOnProducts, pricingCards } from "@/lib/constants";
+
+import { Separator } from "@/components/ui/separator";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { PriceCard } from "./_components/price-card";
+
 interface BillingPageProps {
   params: {
     agencyId: string;
   };
 }
 
-const BillingPage = ({ params }: BillingPageProps) => {
-  return <div>BillingPage</div>;
+const BillingPage = async ({ params }: BillingPageProps) => {
+  // TODO Challenge: Create the add on products
+  const addOns = await stripe.products.list({
+    ids: addOnProducts.map((product) => product.id),
+    expand: ["data.default_price"],
+  });
+
+  const agencySubscription = await db.agency.findUnique({
+    where: {
+      id: params.agencyId,
+    },
+    select: {
+      customerId: true,
+      Subscription: true,
+    },
+  });
+
+  const prices = await stripe.prices.list({
+    product: process.env.NEXT_PANORAMA_PRODUCT_ID,
+    active: true,
+  });
+
+  const currentPlanDetails = pricingCards.find(
+    (c) => c.priceId === agencySubscription?.Subscription?.priceId
+  );
+
+  // charges made for that specific customer
+  const charges = await stripe.charges.list({
+    limit: 50,
+    customer: agencySubscription?.customerId,
+  });
+
+  const allCharges = [
+    ...charges.data.map((charge) => ({
+      description: charge.description,
+      id: charge.id,
+      date: `${new Date(charge.created * 1000).toLocaleTimeString()} ${new Date(
+        charge.created * 1000
+      ).toLocaleDateString()}`,
+      status: "Paid",
+      amount: `$${charge.amount / 100}`,
+    })),
+  ];
+
+  return (
+    <>
+      <h1 className="text-4xl p-4">Billing</h1>
+      <Separator className=" mb-6" />
+      <h2 className="text-2xl p-4">Current Plan</h2>
+      <div className="flex flex-col lg:!flex-row justify-between gap-8">
+        <PriceCard
+          planExists={agencySubscription?.Subscription?.active === true}
+          prices={prices.data}
+          customerId={agencySubscription?.customerId || ""}
+          amount={
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.price || "$0"
+              : "$0"
+          }
+          buttonCta={
+            agencySubscription?.Subscription?.active === true
+              ? "Change Plan"
+              : "Get Started"
+          }
+          highlightDescription="Want to modify your plan? You can do this here. If you have
+          further question contact support@plura-app.com"
+          highlightTitle="Plan Options"
+          description={
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.description || "Lets get started"
+              : "Lets get started! Pick a plan that works best for you."
+          }
+          duration="/ month"
+          features={
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.features || []
+              : currentPlanDetails?.features ||
+                pricingCards.find((pricing) => pricing.title === "Starter")
+                  ?.features ||
+                []
+          }
+          title={
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.title || "Starter"
+              : "Starter"
+          }
+        />
+        {addOns.data.map((addOn) => (
+          <PriceCard
+            planExists={agencySubscription?.Subscription?.active === true}
+            prices={prices.data}
+            customerId={agencySubscription?.customerId || ""}
+            key={addOn.id}
+            amount={
+              //@ts-ignore
+              addOn.default_price?.unit_amount
+                ? //@ts-ignore
+                  `$${addOn.default_price.unit_amount / 100}`
+                : "$0"
+            }
+            buttonCta="Subscribe"
+            description="Dedicated support line & teams channel for support"
+            duration="/ month"
+            features={[]}
+            title={"24/7 priority support"}
+            highlightTitle="Get support now!"
+            highlightDescription="Get priority support and skip the long long with the click of a button."
+          />
+        ))}
+      </div>
+      <h2 className="text-2xl p-4">Payment History</h2>
+      <Table className="bg-card border-[1px] border-border rounded-md">
+        <TableHeader className="rounded-md">
+          <TableRow>
+            <TableHead className="w-[200px]">Description</TableHead>
+            <TableHead className="w-[200px]">Invoice Id</TableHead>
+            <TableHead className="w-[300px]">Date</TableHead>
+            <TableHead className="w-[200px]">Paid</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody className="font-medium truncate">
+          {allCharges.map((charge) => (
+            <TableRow key={charge.id}>
+              <TableCell>{charge.description}</TableCell>
+              <TableCell className="text-muted-foreground">
+                {charge.id}
+              </TableCell>
+              <TableCell>{charge.date}</TableCell>
+              <TableCell>
+                <p
+                  className={cn("", {
+                    "text-emerald-500": charge.status.toLowerCase() === "paid",
+                    "text-orange-600":
+                      charge.status.toLowerCase() === "pending",
+                    "text-red-600": charge.status.toLowerCase() === "failed",
+                  })}
+                >
+                  {charge.status.toUpperCase()}
+                </p>
+              </TableCell>
+              <TableCell className="text-right">{charge.amount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
 };
 
 export default BillingPage;
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -59,7 +59,7 @@ export const pricingCards = [
 ];
 
 export const addOnProducts = [
-  { title: "Priority Support", id: "prod_PNjJAE2EpP16pn" },
+  { title: "Priority Support", id: "prod_PZd5tIEqapUILu" },
 ];
 
 export const icons = [
