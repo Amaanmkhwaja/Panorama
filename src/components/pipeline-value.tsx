dfa4b2f65239f0fd93fb2afa0858c2a90114ba71
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2024-02-19 15:17:14 -0500

    Created Sub Account's Dashboard page

---
+++ src/components/pipeline-value.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+
+import { Prisma } from "@prisma/client";
+import { getPipelines } from "@/actions/pipeline";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Progress } from "@/components/ui/progress";
+
+interface PipelineValueProps {
+  subaccountId: string;
+}
+
+export const PipelineValue = ({ subaccountId }: PipelineValueProps) => {
+  const [pipelines, setPipelines] = useState<
+    Prisma.PromiseReturnType<typeof getPipelines>
+  >([]);
+
+  const [selectedPipelineId, setselectedPipelineId] = useState("");
+  const [pipelineClosedValue, setPipelineClosedValue] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getPipelines(subaccountId);
+      setPipelines(res);
+      setselectedPipelineId(res[0]?.id);
+    };
+    fetchData();
+  }, [subaccountId]);
+
+  const totalPipelineValue = useMemo(() => {
+    if (pipelines.length) {
+      return (
+        pipelines
+          .find((pipeline) => pipeline.id === selectedPipelineId)
+          ?.Lane?.reduce((totalLanes, lane, currentLaneIndex, array) => {
+            const laneTicketsTotal = lane.Tickets.reduce(
+              (totalTickets, ticket) => totalTickets + Number(ticket?.value),
+              0
+            );
+            if (currentLaneIndex === array.length - 1) {
+              setPipelineClosedValue(laneTicketsTotal || 0);
+              return totalLanes;
+            }
+            return totalLanes + laneTicketsTotal;
+          }, 0) || 0
+      );
+    }
+    return 0;
+  }, [selectedPipelineId, pipelines]);
+
+  const pipelineRate = useMemo(
+    () =>
+      (pipelineClosedValue / (totalPipelineValue + pipelineClosedValue)) * 100,
+    [pipelineClosedValue, totalPipelineValue]
+  );
+
+  return (
+    <Card className="relative w-full xl:w-[350px]">
+      <CardHeader>
+        <CardDescription>Pipeline Value</CardDescription>
+        <small className="text-xs text-muted-foreground">
+          Pipeline Progress
+        </small>
+        <div className="flex items-center justify-between">
+          <div>
+            <p className="text-xs text-muted-foreground">
+              Closed ${pipelineClosedValue}
+            </p>
+          </div>
+          <div>
+            <p className="text-xs text-muted-foreground">
+              Total ${totalPipelineValue + pipelineClosedValue}
+            </p>
+          </div>
+        </div>
+        <Progress color="green" value={pipelineRate} className="h-2" />
+      </CardHeader>
+      <CardContent className="text-sm text-muted-foreground">
+        <p className="mb-2">
+          Total value of all tickets in the given pipeline except the last lane.
+          Your last lane is considered your closing lane in every pipeline.
+        </p>
+        <Select
+          value={selectedPipelineId}
+          onValueChange={setselectedPipelineId}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a pipeline" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Pipelines</SelectLabel>
+              {pipelines.map((pipeline) => (
+                <SelectItem value={pipeline.id} key={pipeline.id}>
+                  {pipeline.name}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </CardContent>
+    </Card>
+  );
+};
